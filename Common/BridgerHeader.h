2fe0b2c66eeb969260699b396ff15476a0929359
WeiQijun <175500547@qq.com>
2016-09-04 16:06:46 +0800

    改成cocoapods集成三方库，将所有文件规整在文件夹中。。。。。。。

---
+++ Common/BridgerHeader.h
@@ -0,0 +1,30 @@
+//
+//  BridgerHeader.h
+//  LoveDog
+///Users/apple/Desktop/LoveDog/Common/BridgerHeader.h
+//  Created by wei on 16/8/10.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+///Users/apple/Desktop/LoveDog/Common/BridgerHeader.h
+#ifndef BridgerHeader_h
+#define BridgerHeader_h
+
+//#import "JSONModel.h" //解析json的第三方库
+#import "XTADScrollView.h"
+//#import "UIImageView+WebCache.h"
+//#import "UIButton+WebCache.h"
+#import "HDManager.h"
+//#import "MJRefresh.h"
+#import <CommonCrypto/CommonCrypto.h>//MD5编码用到的头文件
+
+//微信、qq分享及第三方登录 shareSDK
+
+#import <ShareSDK/ShareSDK.h>
+#import <ShareSDKConnector/ShareSDKConnector.h>
+#import <ShareSDKUI/ShareSDK+SSUI.h>
+////腾讯开放平台（对应QQ和QQ空间）SDK头文件
+//#import <TencentOpenAPI/TencentOAuth.h>
+//#import <TencentOpenAPI/QQApiInterface.h>
+//微信SDK头文件
+#import "WXApi.h"
+
+#endif /* BridgerHeader_h */
---
+++ Pods/Target Support Files/Pods-LoveDog/Pods-LoveDog-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_LoveDogVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LoveDogVersionString[];
+
